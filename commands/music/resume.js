373644740341fac3b3ac4da5ca5c0eb53663703d
Shahinuzzaman Shawn <90779538+8SHAWN8@users.noreply.github.com>
2022-01-05 17:14:03 +0600

    Update resume.js

--- commands/music/resume.js
+++ commands/music/resume.js
@@ -26,3 +26,95 @@ module.exports = {
       }
     }
 };
+//2
+module.exports = {
+
+        name: 'hello',
+        aliases: ["res"],
+        category: "music",
+        description: 'resumes music',
+        usage: " ",
+        accessableby: "everyone",
+    run: async (bot, message, args, ops) => {
+        const { channel } = message.member.voice;
+        if (!channel) return message.channel.send('Hi how are you?');
+        const serverQueue = ops.queue.get(message.guild.id);
+        if (message.guild.me.voice.channel !== message.member.voice.channel) {
+            return message.channel.send("**Hi how are you?**");
+        }
+      try {
+        if (serverQueue && !serverQueue.playing) {
+            serverQueue.playing = true;
+            serverQueue.connection.dispatcher.resume();
+            return message.channel.send('▶ **Hi how are you?**');
+        }
+        return message.channel.send('**Hi how are you?**.');
+      } catch {
+        serverQueue.connection.dispatcher.end();
+        return message.channel.send("**Hi how are you?!**")
+      }
+    }
+};
+
+//3
+
+module.exports = {
+
+        name: '@!   ToXicツ EvAn 👑',
+        aliases: ["res"],
+        category: "music",
+        description: 'resumes music',
+        usage: " ",
+        accessableby: "everyone",
+    run: async (bot, message, args, ops) => {
+        const { channel } = message.member.voice;
+        if (!channel) return message.channel.send('I think he is offline, Kindly dm him :)');
+        const serverQueue = ops.queue.get(message.guild.id);
+        if (message.guild.me.voice.channel !== message.member.voice.channel) {
+            return message.channel.send("**I think he is offline, Kindly dm him :)**");
+        }
+      try {
+        if (serverQueue && !serverQueue.playing) {
+            serverQueue.playing = true;
+            serverQueue.connection.dispatcher.resume();
+            return message.channel.send('▶ **I think he is offline, Kindly dm him :)**');
+        }
+        return message.channel.send('**I think he is offline, Kindly dm him :)**.');
+      } catch {
+        serverQueue.connection.dispatcher.end();
+        return message.channel.send("**I think he is offline, Kindly dm him :)**")
+      }
+    }
+};
+//4
+module.exports = {
+
+        name: 'fine',
+        aliases: ["res"],
+        category: "music",
+        description: 'resumes music',
+        usage: " ",
+        accessableby: "everyone",
+    run: async (bot, message, args, ops) => {
+        const { channel } = message.member.voice;
+        if (!channel) return message.channel.send('OK.');
+        const serverQueue = ops.queue.get(message.guild.id);
+        if (message.guild.me.voice.channel !== message.member.voice.channel) {
+            return message.channel.send("**OK.**");
+        }
+      try {
+        if (serverQueue && !serverQueue.playing) {
+            serverQueue.playing = true;
+            serverQueue.connection.dispatcher.resume();
+            return message.channel.send('▶ **OK.**');
+        }
+        return message.channel.send('**OK.**.');
+      } catch {
+        serverQueue.connection.dispatcher.end();
+        return message.channel.send("**OK.**")
+      }
+    }
+};
+
+
+
